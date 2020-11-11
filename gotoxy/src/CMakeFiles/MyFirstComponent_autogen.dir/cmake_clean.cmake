875dcd5b0f539dcbd3095d5d3dfd0a0d58f6ffde
xNOElx24 <nomorenos@alumnos.unex.es>
2020-11-11 10:50:33 +0100

    Gotoxy

--- gotoxy/src/CMakeFiles/MyFirstComponent_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MyFirstComponent_autogen"
-  "MyFirstComponent_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MyFirstComponent_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
